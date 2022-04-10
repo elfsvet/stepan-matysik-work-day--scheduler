dce64cb18496fb81c35d25f761cb8c90738243ab
elfsvet <stepanmatysik@gmail.com>
2022-04-10 17:01:41 -0400

    a lot of logic and new code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,49 @@
-// User Story
-// AS AN employee with a busy schedule
-// I WANT to add important events to a daily planner
-// SO THAT I can manage my time effectively
+// var for fast changing time schedule
+var startWork = 9;
+var endWork = 17;
 
-// Acceptance Criteria
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
-
-
-
-var schedule = [];
-// object to store and update values in localStorage and from what we get as input
-// var container = $(".container");
-// for (var time = 9; time <18; time++){
-//     container.append("<"
-// };
 // the current day displayed at the top of the calendar    
-// to display the day we are at...the header in paragraph
 var getCurrentDate = function () {
     var currentDate = moment().format('dddd, MMMM Do YYYY');
     $("#currentDay").text(currentDate);
 };
 
-
-
-
+// if time > 12 + pm then time - 12 + pm if time <12 time + am
+var checkAmPm = function (time) {
+    if (time > 12) {
+        return time - 12 + ":00 pm";
+    } else if (time === 12) {
+        return time + ":00 pm"
+    } else {
+        return time + ":00 am"
+    }
+};
 
 
 // create time blocks and standard business hours
 // we need to have a container with fields for 9 - 5pm 
-// on the right should be display an hour.
-
-// created hour div
 var createSchedule = function (startTime, endTime) {
     for (var i = startTime; i <= endTime; i++) {
         // creates a text area
         var textArea = $('<textarea>')
-            .addClass('col-md-10 task')
+            .addClass('col-6 col-md-10 task')
+        // i can input text as event
 
         // creates save button
         var saveBtn = $('<button>')
-            .addClass('btn saveBtn col-md-1')
+            .addClass('btn saveBtn col-1 col-md-1')
             // we will change it to save icon
-            .text("Save")
-            .append($("<span>").addClass("iconify").attr("data-icon", "zmdi:floppy"));
-
+            // .text("Save")
+            // i didnt like what iconify stores cache in local storage
+            // .append($("<span>").addClass("iconify").attr("data-icon", "zmdi:floppy"));
+            .append($("<span>").addClass("far fa-save"));
 
 
 
         // creates hour display need to be done in am pm
         var hourDiv = $("<div>")
-            .addClass("col-md-1 hour")
+            .addClass("col-3 col-md-1 hour text-left pl-0")
+            // checks if we need to print ap or pm and change to 12 hour format
             .text(checkAmPm(i))
 
         // creates a container with all time blocks in one for easy adjustments
@@ -77,7 +58,7 @@ var createSchedule = function (startTime, endTime) {
 
 
 
-        // add div in the div with container class
+        // add div textarea and button inside the div that inside the container class
         $(".container").append(hourContainer.append([
             hourDiv,
             textArea,
@@ -86,61 +67,62 @@ var createSchedule = function (startTime, endTime) {
     }
 };
 
+var saveLocal = function () {// this will return time in 24 format where clicked
+    $('.time-block').on('click', '.saveBtn', function () {
+        // get an number (9) of hour from id='hour-9'
+        var idHour = $(this)
+            .parent()
+            .attr('id');
 
+        // gets text from textarea there save btn is pressed
+        var text = $(this)
+            .siblings('.task')
+            .val()
+            .trim();
 
+        // event on click button save. saves text into local storage
+        // saves it to localStorage
+        localStorage.setItem(idHour, text);
+    });
+};
 
-
-
-// the field in past should have grey background
-// the active hour field should be red background
-// the future fields should be in green color
-
-// event on click in the time blocks i can input text as event
-
-// event on click button save. saves text into local storage
-// every field should have a save button to save in localstorage on the left
-
-// refresh the page should keep only saved events.
-
-
-
-// if time >= 12 then time - 12 + pm
-// if time <12 time + am
-var checkAmPm = function (time) {
-    if (time > 12) {
-        return time - 12 + ":00 pm";
-    } else if (time === 12) {
-        return time + ":00 pm"
-    } else {
-        return time + ":00 am"
+var loadTasks = function () {
+    // don't get how to use .each() in jquery so did it this way.
+    // for each hour we have in work day
+    for (var hour = startWork; hour <= endWork; hour++) {
+        //find every div with id hour-(hour number) + class task to target textarea and update it's value from local storage if any information provided with key = "hour-(hour number)".
+        $('#hour-' + hour + " .task").val(localStorage.getItem('hour-' + hour));
     }
-}
+};
 
+// what time is to understand if it past or not yet
+var pastPresentFutureCheck = function () {
+    // using moment.js get current hour to compare in the schedule
+    var currentHour = moment().hour();
+    // for every hour line do function
+    $('.time-block').each(function () {
+        // takes an hour from id makes it number for comparing
+        var blockHour = parseInt($(this).attr("id").split("hour-")[1]);
+        // the field in past should have grey background
+        // check if the time past or not and current
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
 
+            // the active hour field should be red background
+        } else if (blockHour === currentHour) {
+            $(this).addClass("present");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// save the data to localStorage
-
-// load the data function
+            // the future fields should be in green color
+        } else {
+            $(this).addClass("future");
+        }
+    });
+};
 
 // displays the day in the header
 getCurrentDate();
-createSchedule(9, 17);
+createSchedule(startWork, endWork);
+pastPresentFutureCheck();
+loadTasks();
+// save the data to localStorage
+saveLocal();
